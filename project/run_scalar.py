a2841e598eb179ac13c17cf3906754d413b307ca
Сидоров Александр Михайлович <amsidorov_2@edu.hse.ru>
2025-09-30 20:16:17 +0300

    done module 1

--- project/run_scalar.py
+++ project/run_scalar.py
@@ -11,7 +11,9 @@ class Network(minitorch.Module):
     def __init__(self, hidden_layers):
         super().__init__()
         # TODO: Implement for Task 1.5.
-        raise NotImplementedError("Need to implement for Task 1.5")
+        self.layer1 = Linear(2, hidden_layers)
+        self.layer2 = Linear(hidden_layers, hidden_layers)
+        self.layer3 = Linear(hidden_layers,1)
 
     def forward(self, x):
         middle = [h.relu() for h in self.layer1.forward(x)]
@@ -41,8 +43,11 @@ class Linear(minitorch.Module):
 
     def forward(self, inputs):
         # TODO: Implement for Task 1.5.
-        raise NotImplementedError("Need to implement for Task 1.5")
-
+        output = []
+        for j in range(len(self.bias)):
+            weighted_sum = sum(inputs[i] * self.weights[i][j].value for i in range(len(inputs))) + self.bias[j].value
+            output.append(weighted_sum)
+        return output
 
 def default_log_fn(epoch, total_loss, correct, losses):
     print("Epoch ", epoch, " loss ", total_loss, "correct", correct)
